530a053eb2c92a52b002a8520557b03f58c8f783
Paulo Henrique Dalberto <phdalberto2016@gmail.com>
2024-10-12 10:04:36 -0300

    feat: add treatment error

--- src/services/api.ts
+++ src/services/api.ts
@@ -1,8 +1,21 @@
 const apiKey = process.env.NEXT_PUBLIC_API_KEY
 
-export const FetchWeatherData = async (city: string): Promise<WeatherDataProps> => {
-  const url = `https://api.openweathermap.org/data/2.5/weather?q=${city}&APPID=${apiKey}&lang=pt_BR`;
-  const response = await fetch(url);
-  const result = await response.json();
-  return result;
+export const FetchWeatherData = async (city: string, setError: (value: boolean) => void): Promise<WeatherDataProps> => {
+
+  try {
+    const url = `https://api.openweathermap.org/data/2.5/weather?q=${city}&APPID=${apiKey}&lang=pt_BR`;
+    const response = await fetch(url);
+
+    if(!response.ok){
+      setError(true)
+      throw new Error();
+    }
+
+    const result = await response.json();
+    return result;
+  
+  } catch (error) {
+    console.log('erro:', error)
+    throw new Error('erro');
+  }
 }
